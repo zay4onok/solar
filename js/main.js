d52064a2bcf8b49a9fb9f07746fc73a6d6e11125
Dmitry <dmitronus2015@gmail.com>
2024-12-02 21:47:43 +0200

    add green-offer block html + css + js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
-$(function(){
+const tabs = document.querySelectorAll(".green-offer__tab");
+const contents = document.querySelectorAll(".green-offer__content-item");
 
+tabs.forEach((tab) => {
+  tab.addEventListener("click", (event) => {
+    event.preventDefault();
+    tabs.forEach((t) => t.classList.remove("--active"));
+    tab.classList.add("--active");
+
+    const targetSelector = tab.getAttribute("href");
+    const targetElement = document.querySelector(targetSelector);
+
+    contents.forEach((content) => content.classList.remove("--active"));
+    targetElement.classList.add("--active");
+  });
 });
+
