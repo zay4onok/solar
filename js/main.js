0fd5c1a292927bdc5a28b9dd625e1f564c225b45
Dmitry <dmitronus2015@gmail.com>
2024-12-03 14:42:56 +0200

    add clients block js

--- js/main.js
+++ js/main.js
@@ -14,3 +14,10 @@ tabs.forEach((tab) => {
     targetElement.classList.add("--active");
   });
 });
+
+$(document).ready(function () {
+  $(".clients__slider").slick({
+    prevArrow: '<button type="button" class="button slick-prev"><img src="./images/prevArrow.svg" alt="стрелка назад" width="40"></button>',
+    nextArrow: '<button type="button" class="button slick-next"><img src="./images/nextArrow.svg" alt="стрелка вперед" width="40"></button>',
+  });
+});
